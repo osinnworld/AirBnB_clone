c4dfae148a7a99c4210244b0615e20fc34522801
osinnworld <shemogata@gmail.com>
2024-02-08 23:31:21 -0800

    0x03 airbnb

---
+++ tests/test_model/test_base_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""
+Unit tests for the BaseModel class.
+"""
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test cases for the BaseModel class.
+    """
+
+    def test_init(self):
+        """
+        Test initialization of BaseModel instances.
+        """
+        my_model = BaseModel()
+
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """
+        Test the save method of BaseModel instances.
+        """
+        my_model = BaseModel()
+
+        initial_updated_at = my_model.updated_at
+        my_model.save()
+        current_updated_at = my_model.updated_at
+        self.assertNotEqual(initial_updated_at, current_updated_at)
+
+    def test_to_dict(self):
+        """
+        Test the to_dict method of BaseModel instances.
+        """
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict, dict)
+
+        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(my_model_dict['id'], my_model.id)
+        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
+
+    def test_str(self):
+        """
+        Test the string representation (__str__) of BaseModel instances.
+        """
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
+if __name__ == "__main__":
+    unittest.main()
+
